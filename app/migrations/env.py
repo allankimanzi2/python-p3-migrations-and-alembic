f787239df85c231a0984916b224ec29eb3979320
allankimanzi2 <allankim457@gmail.com>
2024-03-16 19:07:07 +0300

    Done assignment

---
+++ app/migrations/env.py
@@ -0,0 +1,5 @@
+# migrations/env.py
+# search file for target_metadata (line 21)
+from your_app.models import Base
+target_metadata = Base.metadata
+
---
+++ app/migrations/migrations/versions/2195098b30a0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2195098b30a0
+Revises: a5088e242ea5
+Create Date: 2024-03-16 19:03:48.826596
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2195098b30a0'
+down_revision = 'a5088e242ea5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
